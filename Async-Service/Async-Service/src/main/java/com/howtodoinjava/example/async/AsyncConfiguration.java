fad356f3884c0db6d97093f58a948f16fffe015b
MercadoJose <97478439+MercadoJose@users.noreply.github.com>
2022-03-11 16:24:39 -0600

    Update AsyncConfiguration.java

--- Async-Service/Async-Service/src/main/java/com/howtodoinjava/example/async/AsyncConfiguration.java
+++ Async-Service/Async-Service/src/main/java/com/howtodoinjava/example/async/AsyncConfiguration.java
@@ -4,11 +4,13 @@ import java.util.concurrent.Executor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
+@Lazy
 public class AsyncConfiguration {
 	@Bean(name = "asyncExecutor")
 	public Executor asyncExecutor() {
